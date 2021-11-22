f00bba66da4bdb12f8c4e2d3ea388ee215fb0dab
Mei-K <mei.kikuchi@rakus-partners.co.jp>
2021-11-22 14:57:22 +0900

    商品一覧表示、検索機能を追加しました

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,80 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import { Item } from "../types/Item";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
+    //商品一覧が入る配列
+    items: new Array<Item>(),
   },
   mutations: {
+    /**
+     * 商品一覧情報を作成してstateに格納する.
+     * @param state - ステートオブジェクト
+     * @param payload - 外部APIから商品一覧情報を取得
+     */
+    getItemList(state, payload) {
+      state.items = new Array<Item>();
+      for (const item of payload.items) {
+        state.items.push(
+          new Item(
+            item.id,
+            item.type,
+            item.name,
+            item.discription,
+            item.priceM,
+            item.priceL,
+            item.imagePath,
+            item.deleteId,
+            item.toppingList
+          )
+        );
+      }
+      console.dir("items:" + JSON.stringify(state.items));
+    },
   },
   actions: {
+    /**
+     * 商品一覧をAPIから取得.
+     * @remarks 取得した商品一覧をJSON形式でペイロードに格納。ミューテーションからgetItemListメソッドを呼び出してオブジェクト化している
+     * @param context - コンテキスト
+     *
+     */
+    async getItemList(context) {
+      const responce = await axios.get(
+        "http://153.127.48.168:8080/ecsite-api/item/items/coffee"
+      );
+      console.dir("responce:" + JSON.stringify(responce));
+      const payload = responce.data;
+
+      //(memo)ミューテーションから呼び出している
+      context.commit("getItemList", payload);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+  getters: {
+    /**
+     * 商品一覧を取得する.
+     * @param state - ステートオブジェクト
+     * @returns - 商品一覧
+     */
+    getAllItems(state) {
+      return state.items;
+    },
+
+    /**
+     * 検索欄で入力されたキーワードで商品を絞り込む
+     *@param state - ステートオブジェクト
+     *@returns - 曖昧検索で絞り込まれた商品
+     */
+    getSerchKeyWord(state) {
+      return (keyWord: string) => {
+        return state.items.filter((item) => item.name.includes(keyWord));
+      };
+    },
+  },
+});
