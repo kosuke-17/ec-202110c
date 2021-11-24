a4208b04c7871d86c2c1ad44646683b1054d37aa
kosuke-17 <tamutamu17@icloud.com>
2021-11-25 08:48:25 +0900

    お問い合わせ機能の実装

--- src/views/contactCompany.vue
+++ src/views/contactCompany.vue
@@ -8,7 +8,7 @@
           <div class="row">
             <div class="input-field col s12">
               <i class="material-icons prefix">account_circle</i>
-              <input class="validate" id="name" type="email" v-model="name" />
+              <input class="validate" id="name" type="text" v-model="name" />
               <label for="name" data-error="wrong" data-success="right"
                 >お名前</label
               >
@@ -17,13 +17,8 @@
           <div class="row">
             <div class="input-field col s12">
               <i class="material-icons prefix">mail_outline</i>
-              <input
-                class="validate"
-                id="mailAddress"
-                type="email"
-                v-model="mailAddress"
-              />
-              <label for="mailAddress" data-error="wrong" data-success="right"
+              <input class="validate" id="email" type="email" v-model="email" />
+              <label for="email" data-error="wrong" data-success="right"
                 >メールアドレス</label
               >
             </div>
@@ -42,7 +37,7 @@
             </div>
           </div>
           <div class="row login-btn">
-            <button class="btn" type="button">
+            <button class="btn" type="button" @click="submitToCompany">
               <span>送信</span>
             </button>
           </div>
@@ -52,8 +47,62 @@
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {
+  private name = "";
+  private email = "";
+  private content = "";
+
+  async submitToCompany(): Promise<void> {
+    const payload = {
+      text:
+        "お問い合わせがありました\n" +
+        "お名前: " +
+        this.name +
+        "\n" +
+        "メールアドレス: " +
+        this.email +
+        "\n" +
+        "【問い合わせ内容】\n" +
+        this.content,
+    };
+    const url =
+      "https://hooks.slack.com/services/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";
+    if (this.name && this.email && this.content) {
+      await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+      })
+        .then((data) => {
+          console.log("Success:", data);
+          alert("送信が完了しました。");
+          this.name = "";
+          this.email = "";
+          this.content = "";
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    } else {
+      alert("お問い合わせ内容は全て入力してください");
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+</style>
