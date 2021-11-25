3a5140f198a4fa4b095b224f9259af484120592d
Mei-K <mei.kikuchi@rakus-partners.co.jp>
2021-11-25 09:55:38 +0900

    ヘッダーのアイコン、配置を変更しました

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,20 +12,20 @@
           <router-link to="/itemList">
             <i class="fas fa-utensils"></i> 商品一覧
           </router-link>
-          <router-link to="/registerUser" v-if="loginStatus === false">
-            <i class="fas fa-user"></i>会員登録
-          </router-link>
           <router-link to="/cartList">
             <i class="fas fa-shopping-cart"></i>カート
+          </router-link>
+          <router-link to="/contactCompany">
+            <i class="fas fa-comment"></i>お問い合わせ
+          </router-link>
+          <router-link to="/registerUser" v-if="loginStatus === false">
+            <i class="fas fa-user"></i>会員登録
           </router-link>
           <router-link to="/loginUser" v-if="loginStatus === false">
             <i class="fas fa-sign-in-alt"></i>ログイン
           </router-link>
           <router-link to="/logoutUser" v-if="loginStatus === true">
             <i class="fas fa-sign-out-alt"></i>ログアウト
-          </router-link>
-          <router-link to="/contactCompany">
-            <i class="fas fa-sign-out-alt"></i>お問い合わせ
           </router-link>
         </div>
       </div>
