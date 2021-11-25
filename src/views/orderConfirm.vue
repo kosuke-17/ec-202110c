0aec28a86bd3f46e4c2a4f20f8b46fe6c167b4d2
hanawtnb <hanawtnb@icloud.com>
2021-11-25 11:00:11 +0900

    クレジットカード情報の入力欄を追加しました。

--- src/views/orderConfirm.vue
+++ src/views/orderConfirm.vue
@@ -208,6 +208,75 @@
           <span>クレジットカード</span>
         </label>
       </span>
+      <div class="creditCard" v-if="paymentMethod == 2">
+        <div class="row creditNum">
+          <div class="input-field col s12">
+            <input
+              id="creditNum"
+              type="number"
+              class="validate"
+              oninput="javascript:if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+              maxlength="16"
+            />
+            <label for="creditNum">クレジットカード番号</label>
+          </div>
+        </div>
+
+        <div class="expiryDate">
+          <div>
+            <label for="expiryDate">有効期限：</label>
+            <select class="browser-default expiryMonth">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>
+          </div>
+          <div>
+            <br />
+            <label></label>
+            <select class="browser-default expiryMonth">
+              <option value="2020">2020</option>
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+              <option value="2026">2026</option>
+              <option value="2027">2027</option>
+              <option value="2028">2028</option>
+              <option value="2029">2029</option>
+              <option value="2030">2030</option>
+            </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="creditName" type="text" class="validate" />
+            <label for="creditName">カード名義人</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input
+              id="securityCode"
+              type="number"
+              class="validate"
+              oninput="javascript:if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+              maxlength="4"
+            />
+            <label for="securityCode">セキュリティコード</label>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="row order-confirm-btn">
       <button class="btn" type="button" @click="orderInfomation">
@@ -350,3 +419,51 @@ export default class OrderConfirm extends Vue {
   }
 }
 </script>
+
+<style scoped>
+@charset "UTF-8";
+
+.order-confirm-delivery-info {
+  margin: 0 200px 0 200px;
+}
+
+.order-confirm-delivery-datetime {
+  text-align: center;
+}
+
+.order-confirm-delivery-time {
+  margin-right: 10px;
+}
+
+.order-confirm-payment-method {
+  text-align: center;
+  width: 60%;
+  margin: 0 auto;
+}
+
+.order-confirm-payment-method-radio {
+  margin-right: 10px;
+}
+.expiryDate {
+  display: flex;
+}
+
+.cardCredit {
+  text-align: center;
+}
+
+.expiryDate {
+  margin-bottom: 20px;
+}
+
+.order-confirm-btn {
+  text-align: center;
+  margin-top: 20px;
+}
+
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>
