1eab62571a3db0cf62197f939da41e7133aed4e9
igamasayuki <igamasayuki@gmail.com>
2021-11-30 10:35:08 +0900

    起動時に出ていたエラーを出ないように修正しました

--- src/views/notFound.vue
+++ src/views/notFound.vue
@@ -5,7 +5,11 @@
     </div>
   </div>
 </template>
-<script></script>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+@Component
+export default class notFound extends Vue {}
+</script>
 <style scoped>
 div {
   background: #def2ff;
